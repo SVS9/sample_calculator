aa6d693305db53c1f0d58945b46f513ee36b07cd
Kishoredevops9 <87264592+Kishoredevops9@users.noreply.github.com>
2022-05-15 22:23:56 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,28 +1,31 @@
 pipeline {  
-    agent any
-    environment{
-        path=' C:/Program Files/apache-maven-3.8.5-bin/apache-maven-3.8.5/bin'
-    }
+    agent any  
     stages {  
             stage ('Git-Checkout') {  
                 steps{
-                    git credentialsId: 'e6757c46-0750-4ab1-8cd4-c569efcbbdb1', url: 'https://github.com/Kishoredevops9/Pipeline-project-deployment-on-Tomcat.git'
+                    git url: 'https://github.com/Kishoredevops9/Pipeline-project-deployment-on-Tomcat.git'
                     echo "Checkout successful";
                 } 
             }
             stage ('Compile') {  
                   steps{
-                    sh 'mvn compile'
+                    bat label: '', script: 'mvn compile'
+                    echo "test successful";
+                    
+                } 
+            }
+            stage ('Build') {  
+                  steps{
+                    bat label: '', script: 'mvn clean'
+                    bat label: '', script: 'mvn package'
+                    echo "build successful";
+                    
+                } 
+            }
+             stage ('Test') {  
+                  steps{
+                    bat label: '', script: 'mvn test'
                     echo "test successful";
                 } 
             }
-             stage ('Build') {  
-                  steps{
-                    sh 'mvn clean package'
-                    echo "build successful";
-                } 
-            }
             
-        }
-      
-   }
